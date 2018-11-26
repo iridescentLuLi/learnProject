45640028d1fcdb917cb943d22b8738b68b8f7772
liushuang <517085537@qq.com>
2018-11-26 12:37:44 +0800

    again

---
+++ drawer/src/com/mr/draw/DrawPictureCanvas.java
@@ -0,0 +1,8 @@
+package com.mr.draw;
+
+import java.awt.*;
+
+public class DrawPictureCanvas extends Canvas {
+    private Image image = null; // 创建画板中的图片对象
+
+}
--- drawer/src/com/mr/draw/DrawPictureFrame.java
+++ drawer/src/com/mr/draw/DrawPictureFrame.java
@@ -1,4 +1,18 @@
 package com.mr.draw;
 
-public class DrawPictureFrame {
+import javax.swing.*;
+
+public class DrawPictureFrame extends JFrame {
+    public DrawPictureFrame() {
+        setResizable(false); // 窗体不能改变大小；
+        setTitle("DrawPicture"); // 标题
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //窗体关闭则停止程序
+        setBounds( 500, 100, 574, 460);//位置大小
+    }
+
+    public static void main(String[] args){
+        DrawPictureFrame frame = new DrawPictureFrame();
+        frame.setVisible(true); //窗体可见
+    }
 }
+
